16a7eeb783ef12e9b009823a35fb6c2afaccfa80
MOWEN19 <111128692+MOWEN19@users.noreply.github.com>
2023-04-01 17:16:46 -0300

    Creacion de Clases, metodos setter y getter, metodos basicos, division de archivos para ordenamiento, entrada de datos correcta.

---
+++ src/main/java/Terminal.java
@@ -0,0 +1,48 @@
+import java.util.HashMap;
+
+  public class Terminal{
+    private int cantBuses;
+    //llave destino y valor el bus (?) 
+    HashMap<String, Bus> andenes;
+    
+    public Terminal(){
+      this.andenes = new HashMap();
+        
+      this.cantBuses = 0;
+      //System.out.println("cantBuses: "+andenes.size());
+    }   
+
+    public void agregarBus(String destino, Bus bus){
+        
+      if(andenes==null){
+          andenes = new Hashmap();
+          andenes.put(destino,bus);
+          return;
+      }  
+      if(andenes.size()>10){
+        System.out.println("Capacidad llena");
+        return;
+      }
+      if(!andenes.containsKey(destino)){
+        andenes.put(destino,bus);
+        System.out.println("cantBuses: "+andenes.size());
+      }
+    }
+
+    private static class Hashmap extends HashMap<String, Bus> {
+
+        public Hashmap() {
+        }
+    }
+    
+  }
+  
+
+
+    
+
+
+    
+
+    
+  
