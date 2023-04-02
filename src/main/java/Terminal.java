e82abb1577886bd195b5fde06db75833996eaf26
MOWEN19 <111128692+MOWEN19@users.noreply.github.com>
2023-04-02 16:40:39 -0400

    Sobrecarga de metodo agregarBus.

--- src/main/java/Terminal.java
+++ src/main/java/Terminal.java
@@ -1,14 +1,14 @@
 import java.util.HashMap;
 
   public class Terminal{
-    private int cantBuses;
+    //private int cantBuses;
     //llave destino y valor el bus (?) 
     HashMap<String, Bus> andenes;
     
     public Terminal(){
       this.andenes = new HashMap();
         
-      this.cantBuses = 0;
+      //this.cantBuses = 0;
       //System.out.println("cantBuses: "+andenes.size());
     }   
 
@@ -28,13 +28,29 @@ import java.util.HashMap;
         System.out.println("cantBuses: "+andenes.size());
       }
     }
-
+    
+    
+    public void agregarBus(String destino){
+      Bus BB = new Bus(destino);
+      if(andenes==null){
+          andenes = new Hashmap();
+          andenes.put(destino,BB);
+          return;
+      }  
+      if(andenes.size()>10){
+        System.out.println("Capacidad llena");
+        return;
+      }
+      if(!andenes.containsKey(destino)){
+        andenes.put(destino,BB);
+        System.out.println("cantBuses: "+andenes.size());
+      }
+    }
+    
     private static class Hashmap extends HashMap<String, Bus> {
-
         public Hashmap() {
         }
     }
-    
   }
   
 
